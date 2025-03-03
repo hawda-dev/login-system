182f00355aa90916497f1557f1f7240d7509eb9c
hawda-dev <hadihawda566@gmail.com>
2025-03-03 17:32:25 +0300

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,14 @@
 function check(form) {
-    username = new Array("admin1", "admin2", "members");
-    password = new Array("admin1", "admin2", "members");
+    username = new Array("admin1", "admin2", "members", "hawda@hawda.store");
+    password = new Array("admin1", "admin2", "members", "hawda1234");
 
 	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
 		location.replace('#adminhome')/* The Administrator homepage */
 	}
 	else if(form.userid.value == username[2] && form.pswrd.value == password[2]) {
+		location.replace('members/home.html')/* The members homepage */
+	}
+		else if(form.userid.value == username[3] && form.pswrd.value == password[3]) {
 		location.replace('members/home.html')/* The members homepage */
 	}
 	else if(form.userid.value == username[0]||form.userid.value == username[1]||form.userid.value == username[2]) {
